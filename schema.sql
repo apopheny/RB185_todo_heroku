18436ab4c074889f874cb76ae8dc520e99a81469
Chris Douglass <cedouglass@gmail.com>
2023-03-07 12:07:40 -0500

    added db schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE lists(
+  id serial PRIMARY KEY,
+  name text UNIQUE NOT NULL
+);
+
+CREATE TABLE todos(
+  id serial PRIMARY KEY,
+  list_id int NOT NULL
+    REFERENCES lists (id),
+  name text NOT NULL,
+  completed boolean NOT NULL DEFAULT FALSE
+);
